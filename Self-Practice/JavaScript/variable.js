911b13e0a051cc9bf72ab33f71a2e25e820bd711
alaminnhasan <alaminedu.cse@gmail.com>
2024-01-09 17:32:02 +0600

    update

---
+++ Self-Practice/JavaScript/variable.js
@@ -0,0 +1,21 @@
+let intVal = 23             //syntax: var/let/const varName = value
+console.log(intVal)
+console.log(typeof intVal)  //typeof - showed the type of variable
+
+console.log()
+
+let floatVal = 1.734
+console.log(floatVal)
+console.log(typeof floatVal)        //int/float both r in number type
+
+console.log()
+
+let strVal = "this is string"
+console.log(strVal)
+console.log(typeof strVal)
+
+console.log()
+
+let boolVal = true
+console.log(boolVal)
+console.log(typeof boolVal)
